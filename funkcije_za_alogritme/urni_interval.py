e7abd69d425520eb8fff70c2944d2ad60588c056
LukaRiz <92185057+LukaRiz@users.noreply.github.com>
2024-04-06 22:57:15 +0200

    Update 1.0.4.

--- funkcije_za_alogritme/urni_interval.py
+++ funkcije_za_alogritme/urni_interval.py
@@ -4,17 +4,26 @@ def odpri_datoteko(ime_datoteke):
     with open(ime_datoteke, 'r') as datoteka:
         return datoteka.readlines()
 
-#djijdi
-#zacetek funkcije za urni interval za računanje koliko narašča na uro posamezna reka
 def urni_interval(ime_datoteke):
     datoteka = odpri_datoteko(ime_datoteke)
     prejsnji_cas = None
-    skupno_minut = 0
+    skupno_ur = 0
 
     for vrstica in reversed(datoteka):
-        datum_cas, vodostaj, pretok, temp = vrstica.split(",")
-        datum, cas = datum_cas.split(" ")
-        print(datum, cas)
+        datum_cas, _, _, _ = vrstica.split(",")
+        _, cas = datum_cas.split(" ")
+        trenutni_cas = datetime.strptime(datum_cas, "%d.%m.%Y %H:%M")
 
+        if prejsnji_cas is not None:
+            casovna_razlika = trenutni_cas - prejsnji_cas
+            skupno_ur += casovna_razlika.total_seconds() / 3600
 
-print(urni_interval("example7.csv"))
+            # Izpis polne ure
+            if int(skupno_ur) > int(skupno_ur - casovna_razlika.total_seconds() / 3600):
+                print(f"{int(skupno_ur)} ura")
+
+        prejsnji_cas = trenutni_cas
+
+    return skupno_ur
+
+print(urni_interval("/Users/lukarizman/pythonProject/TestiranjeKode/datoteke_za_testiranje/example7.csv"))
